2885fd647f6790ecdb650b0737efca60f7fd5caf
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2025-09-08 19:34:47 -0300

    Criando testes de repositório do método Get

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -3,13 +3,17 @@ package db_test
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/leomoritz/fullcycle-arq-hexagonal-app/adapters/db"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
 
 // Função parar criar o processo de conexão com o banco de dados
 func setup() {
-	Db, _ = sql.Open("slite3", ":memory:") // o arquivo fica em memória apenas para teste (mais rápido)
+	Db, _ = sql.Open("sqlite3", ":memory:") // o arquivo fica em memória apenas para teste (mais rápido)
 	createTable(Db)
 	createProduct(Db)
 }
@@ -35,4 +39,16 @@ func createProduct(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setup()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+
 }

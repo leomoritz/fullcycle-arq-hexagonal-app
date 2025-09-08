ea3944d849a2d1e99e7aecff04ce6d867e119266
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2025-09-08 12:57:45 -0300

    Finalizando implementação do service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -28,3 +28,68 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)               // Deve retornar nulo para erro
 	require.Equal(t, product, result) // Deve retornar o resultado que deve ser igual ao produto mockado
 }
+
+func TestProductService_Create(t *testing.T) {
+	// Cria o mock da dependência externa
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish() // O defer serve para executar depois que tudo estiver terminado
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	// Cria o service injetando o mock como dependência
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("Product 1", 10)
+	require.Nil(t, err)               // Não deve retornar erros
+	require.Equal(t, product, result) // Deve retornar o resultado que deve ser igual ao produto mockado
+}
+
+func TestProductService_Enable(t *testing.T) {
+	// Cria o mock da dependência externa
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish() // O defer serve para executar depois que tudo estiver terminado
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil).AnyTimes()
+	product.EXPECT().GetStatus().Return("enabled")
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	// Cria o service injetando o mock como dependência
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)               // Não deve retornar erros
+	require.Equal(t, product, result) // Deve retornar o resultado que deve ser igual ao produto mockado
+	require.Equal(t, result.GetStatus(), "enabled")
+}
+
+func TestProductService_Disable(t *testing.T) {
+	// Cria o mock da dependência externa
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish() // O defer serve para executar depois que tudo estiver terminado
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(nil).AnyTimes()
+	product.EXPECT().GetStatus().Return("disabled")
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	// Cria o service injetando o mock como dependência
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Disable(product)
+	require.Nil(t, err)               // Não deve retornar erros
+	require.Equal(t, product, result) // Deve retornar o resultado que deve ser igual ao produto mockado
+	require.Equal(t, result.GetStatus(), "disabled")
+}

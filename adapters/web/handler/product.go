5bacd4f023a1abb9bffdf801609a0deea183ce8d
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2025-09-09 14:40:09 -0300

    Implementando handlers para as últimas ações Enable e Disable products

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -18,6 +18,14 @@ func MakeProductHandlers(router *mux.Router, middleware *negroni.Negroni, servic
 	router.Handle("/product", middleware.With(
 		negroni.Wrap(createProduct(service)),
 	)).Methods("POST", "OPTIONS")
+
+	router.Handle("/product/{id}/enable", middleware.With(
+		negroni.Wrap(enableProduct(service)),
+	)).Methods("PUT", "OPTIONS")
+
+	router.Handle("/product/{id}/disable", middleware.With(
+		negroni.Wrap(disableProduct(service)),
+	)).Methods("PUT", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -71,5 +79,69 @@ func createProduct(service application.ProductServiceInterface) http.Handler {
 			return
 		}
 		response.WriteHeader(http.StatusOK)
+	})
+}
+
+func enableProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("Content-Type", "application/json")
+
+		vars := mux.Vars(request) // pega todas as variáveis da requisição
+		id := vars["id"]
+
+		product, err := service.Get(id)
+		if err != nil {
+			response.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if product == nil {
+			response.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		result, err := service.Enable(product)
+		if err != nil {
+			response.WriteHeader(http.StatusBadRequest)
+			response.Write(JsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(response).Encode(result)
+		if err != nil {
+			response.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func disableProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("Content-Type", "application/json")
+
+		vars := mux.Vars(request) // pega todas as variáveis da requisição
+		id := vars["id"]
+
+		product, err := service.Get(id)
+		if err != nil {
+			response.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if product == nil {
+			response.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		result, err := service.Disable(product)
+		if err != nil {
+			response.WriteHeader(http.StatusBadRequest)
+			response.Write(JsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(response).Encode(result)
+		if err != nil {
+			response.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	})
 }
